7a14df042133e546b83b69cf4aa5dda1ccaf3868
Rubens Fernando <ola@rubensfernando.com>
2015-06-22 11:54:43 -0300

    Update Title

--- static/publish-embed.js
+++ static/publish-embed.js
@@ -1,85 +1,85 @@
 require(['dw/chart/publish'], function() {
 
-    $(function() {
+  $(function() {
 
-        var action = $('.chart-actions .action-publish-embed'),
-            modal;
+    var action = $('.chart-actions .action-publish-embed'),
+        modal;
 
-        $('a', action).click(function(e) {
-            e.preventDefault();
-            showModal();
+    $('a', action).click(function(e) {
+      e.preventDefault();
+      showModal();
+    });
+
+    var showModal = function() {
+      var chart = dw.backend.currentChart,
+          chart_id = chart.get('id'),
+          chart_url = 'http://' + dw.backend.__chartCacheDomain + '/' + chart_id + '/',
+          w = chart.get('metadata.publish.embed-width'),
+          h = chart.get('metadata.publish.embed-height'),
+          titleChart = chart.get('title');
+
+      $.get('/plugins/publish-embed/publish-embed.twig', function(data) {
+
+        modal = $('<div class="publish-chart-action-modal modal hide">' + data + '</div>').modal();
+
+        publish_chart(function(err) {
+          if (err) throw err;
+
+          var textileCode = 'p(artHtml5). "[Infográfcos {{titleChart}} - Crédito: Editoria de Arte/Folhapress]":{{urlChart}}?w={{w}}&h={{h}}',
+              htmlCode = '<iframe src="{{urlChart}}"  width="{{w}}" height="{{h}}" frameborder="0"  allowtransparency="true"  allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen" oallowfullscreen="oallowfullscreen" msallowfullscreen="msallowfullscreen"></iframe> ',
+              urlCode = '{{urlChart}}';
+
+          textileCode = textileCode.replace('{{urlChart}}', chart_url)
+              .replace('{{titleChart}}', titleChart)
+              .replace('{{w}}', w)
+              .replace('{{h}}', h);
+          htmlCode = htmlCode.replace('{{urlChart}}', chart_url)
+              .replace('{{w}}', w)
+              .replace('{{h}}', h);
+
+          urlCode = urlCode.replace('{{urlChart}}', chart_url);
+
+          $('.codes').delay(1000).fadeIn('fast');
+          $('.loading').delay(1000).fadeOut('fast');
+
+          $('textarea#textileArea').html(textileCode);
+          $('textarea#htmlArea').html(htmlCode);
+          $('textarea#urlArea').html(urlCode);
         });
 
-        var showModal = function() {
-            var chart = dw.backend.currentChart,
-                chart_id = chart.get('id'),
-                chart_url = 'http://' + dw.backend.__chartCacheDomain + '/' + chart_id + '/',
-                w = chart.get('metadata.publish.embed-width'),
-                h = chart.get('metadata.publish.embed-height'),
-                titleChart = chart.get('title');
+      });
 
+      console.log(chart_url);
 
+      function publish_chart(callback) {
+        var pending = true;
 
-            $.get('/plugins/publish-embed/publish-embed.twig', function(data) {
+        if (chart.get('publishedAt') && chart.get('publishedAt') > chart.get('lastModifiedAt')) {
+          // no change since last publish
+          return callback(null);
+        }
 
-                modal = $('<div class="publish-chart-action-modal modal hide">' + data + '</div>').modal();
+        $.ajax({
+          url: '/api/charts/' + chart_id + '/publish?local=1',
+          type: 'post'
+        }).done(function() {
+          callback(null);
+        }).fail(function() {
+          console.error('failed');
+          callback('publish failed');
+          pending = false;
+        });
+        // in the meantime, check status periodically
+        checkStatus();
 
-                publish_chart(function(err) {
-                    if (err) throw err;
+        function checkStatus() {
+          $.getJSON('/api/charts/' + chart_id + '/publish/status', function(res) {
+            if (pending) setTimeout(checkStatus, 300);
+          });
+        }
+      };
+    };
 
-                    var textileCode = 'p(artHtml5). "[{titleChart} - Crédito: Editoria de Arte/Folhapress]":{urlChart}?w={w}&h={h}',
-                        htmlCode = '<iframe src="{urlChart}" frameborder="0"  allowtransparency="true"  allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen" oallowfullscreen="oallowfullscreen" msallowfullscreen="msallowfullscreen" width="{w}" height="{h}"></iframe> ';
-
-                    textileCode = textileCode.replace('{urlChart}', chart_url)
-                        .replace('{titleChart}', titleChart)
-                        .replace('{w}', w)
-                        .replace('{h}', h);
-                    htmlCode = htmlCode.replace('{urlChart}', chart_url)
-                        .replace('{w}', w)
-                        .replace('{h}', h);
-
-                    $('.codes').delay(1000).fadeIn('fast');
-                    $('.loading').delay(1000).fadeOut('fast');
-
-                    $('textarea#textileArea').html(textileCode);
-                    $('textarea#htmlArea').html(htmlCode);
-                });
-
-            });
-
-
-
-            console.log(chart_url);
-
-            function publish_chart(callback) {
-                var pending = true;
-
-                if (chart.get('publishedAt') && chart.get('publishedAt') > chart.get('lastModifiedAt')) {
-                    // no change since last publish
-                    return callback(null);
-                }
-
-                $.ajax({
-                    url: '/api/charts/' + chart_id + '/publish?local=1',
-                    type: 'post'
-                }).done(function() {
-                    callback(null);
-                }).fail(function() {
-                    console.error('failed');
-                    callback('publish failed');
-                    pending = false;
-                });
-                // in the meantime, check status periodically
-                checkStatus();
-
-                function checkStatus() {
-                    $.getJSON('/api/charts/' + chart_id + '/publish/status', function(res) {
-                        if (pending) setTimeout(checkStatus, 300);
-                    });
-                }
-            };
-        };
-
-    });
+  });
 
 });
